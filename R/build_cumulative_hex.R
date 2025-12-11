070f6d8a30374eb91aa65f91288a666c3c1647da
Evelyne Brie <32559774+evelynebrie@users.noreply.github.com>
2025-12-10 21:10:43 -0500

    Update build_cumulative_hex.R
    output_dir <- normalizePath("public", mustWork = FALSE)

--- R/build_cumulative_hex.R
+++ R/build_cumulative_hex.R
@@ -12,7 +12,7 @@ start_time <- Sys.time()
 
 # Config
 data_path <- "data/daily"
-output_dir <- "public"
+output_dir <- normalizePath("public", mustWork = FALSE)
 HEX_SIZE <- 5000  # 5km hexes
 SIMPLIFY <- 200   # 200m simplification
 
