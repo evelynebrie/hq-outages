3d47627a5529ca82edbd7bced81d6378b632c1ce
Evelyne Brie <32559774+evelynebrie@users.noreply.github.com>
2026-04-22 11:03:36 -0400

    Fix '1 occurrence: panne de 15 minutes' wording in both versions

--- R/build_cumulative_hex_15min.R
+++ R/build_cumulative_hex_15min.R
@@ -1755,7 +1755,7 @@ cat(' km</div>" +
 cat(num_days, file = html_file, sep = "")
 
 cat('</div>" +
-                    "<div>1 occurrence: 1 panne de 15 minutes</div>" +
+                    "<div>1 occurrence: panne de 15 minutes</div>" +
                 "</div>";
             return div;
         };
